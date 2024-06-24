d1d253dc62a4690afb255d841ed675e3cc60e343
Ashish Aggarwal <ashu2000aggarwal@gmail.com>
2024-06-24 11:48:34 +0530

    First code adding

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'CSPRO Technology',
+  email: 'admin@cspro.com',
+  photoURL: '/favicon/favicon.ico',
+};
